e34eca2b637436e2f8ef44e82c23907f792c2d6e
Alec Jones <alecaej2002@gmail.com>
2020-08-25 19:15:00 -0700

    can now sign in with twitter!

---
+++ app/controllers/callbacks_controller.rb
@@ -0,0 +1,8 @@
+#app/controllers/sessions_controller.rb
+ 
+class CallbacksController < Devise::OmniauthCallbacksController
+  def twitter
+      @user = User.from_omniauth(request.env["omniauth.auth"])
+      sign_in_and_redirect @user
+  end
+end
